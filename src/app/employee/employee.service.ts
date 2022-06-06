0b34a9f6fe0311de82177c20436e891b107f5502
manishay123 <106860254+manishay123@users.noreply.github.com>
2022-06-06 14:56:32 +0530

    config

---
+++ src/app/employee/employee.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http:HttpClient) { }
+  getEmployees():Observable<Employee[]>{
+    return this.http.get<Employee[]>()
+  }
+}
